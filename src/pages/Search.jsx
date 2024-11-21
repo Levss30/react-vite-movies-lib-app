e613dd952da597c6e161b063f58bf5265077ada2
Lucas <lucas.levasseur18@gmail.com>
2024-11-20 21:51:56 -0300

    Feat Const and Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
-import "./movieGrid.css"
+import "./moviesGrid.css"
 
 const searchURL = import.meta.env.VITE_SEARCH
-const apikey = import.meta.env.VITE_API_KEY
+const apiKey = import.meta.env.VITE_API_KEY
 
 const Search = () => {
   const [searchParams] = useSearchParams()
@@ -12,12 +12,25 @@ const Search = () => {
   const[movies, setMovies] = useState([]);
   const query = searchParams.get("q");
 
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setMovies(data.results)
+};
+
+useEffect(() => {
+const searchWithQueryURL = `${searchURL}?${apiKey}&query=${query}`;
+
+getSearchedMovies(searchWithQueryURL);
+}, [query]) 
+
   return (
     <div className='container'>
       <h2 className='title'>Resultados para: <span className="query-text">{query}</span></h2>
       <div className="movies-container">
-        {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        {movies.length === 0 && <p>Carregando...</p>}
+        {movies.length && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
   )
