97745478ffc5636ca7499cf9c8b11dc1ad79f5a6
Lucas <lucas.levasseur18@gmail.com>
2024-11-20 21:45:14 -0300

    Feat Search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+import "./movieGrid.css"
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apikey = import.meta.env.VITE_API_KEY
+
 const Search = () => {
+  const [searchParams] = useSearchParams()
+
+  const[movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
   return (
-    <div>Search</div>
+    <div className='container'>
+      <h2 className='title'>Resultados para: <span className="query-text">{query}</span></h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
   )
 }
 
